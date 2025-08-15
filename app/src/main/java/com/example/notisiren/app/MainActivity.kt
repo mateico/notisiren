447febcc1f3eb9e71941057dc3a982a02b91a9de
Mateo <mateo.rial@gmail.com>
2025-08-14 22:05:23 -0300

    permission granted

--- app/src/main/java/com/example/notisiren/app/MainActivity.kt
+++ app/src/main/java/com/example/notisiren/app/MainActivity.kt
@@ -1,6 +1,9 @@
 package com.example.notisiren.app
 
+import android.Manifest
 import android.content.Intent
+import android.content.pm.PackageManager
+import android.os.Build
 import android.os.Bundle
 import android.provider.Settings
 import androidx.activity.ComponentActivity
@@ -15,6 +18,8 @@ import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
 import com.example.notisiren.ui.NotiSirenEffect
 import com.example.notisiren.ui.NotiSirenViewModel
 import com.example.notisiren.ui.NotificationAlarmScreen
@@ -23,10 +28,17 @@ import dagger.hilt.android.AndroidEntryPoint
 @AndroidEntryPoint
 class MainActivity : ComponentActivity() {
 
+    companion object {
+        private const val REQUEST_NOTIFICATION_PERMISSION_CODE = 1001
+    }
+
     private val viewModel: NotiSirenViewModel by viewModels()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        requestNotificationPermissionIfNeeded()
+
         setContent {
             val state by viewModel.state.collectAsState()
             val snackbarHostState = remember { SnackbarHostState() }
@@ -50,6 +62,21 @@ class MainActivity : ComponentActivity() {
                     state = state,
                     onEvent = viewModel::onEvent,
                     modifier = Modifier.Companion.padding(innerPadding)
+                )
+            }
+        }
+    }
+
+    private fun requestNotificationPermissionIfNeeded() {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU) {
+            if (ContextCompat.checkSelfPermission(
+                this,
+                    Manifest.permission.POST_NOTIFICATIONS
+            ) != PackageManager.PERMISSION_GRANTED) {
+                ActivityCompat.requestPermissions(
+                    this,
+                    arrayOf(Manifest.permission.POST_NOTIFICATIONS),
+                    REQUEST_NOTIFICATION_PERMISSION_CODE
                 )
             }
         }
